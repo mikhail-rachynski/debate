4cd5ba63fe4f514606be693d2ba8aaedd239557e
MIkhail Rachynski <mixasun@gmail.com>
2021-02-23 21:54:33 +0300

    created project template

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import User from "./components/User/User";
 import Login from "./components/Login/Login";
 
 function App() {
-  return (
-      <div className="App">
-        <Header />
-        <div>
-          <Route exact path='/' render={() => <Start /> } />
-          <Route path='/users' render={() => <User /> } />
-          <Route path='/login' render={() => <Login /> } />
-        </div>
+    return (
+        <div className="App">
+            <Header />
+            <div>
+                <Route exact path='/' render={() => <Start /> } />
+                <Route path='/users' render={() => <User /> } />
+                <Route path='/login' render={() => <Login /> } />
+            </div>
 
-      </div>
-  );
+        </div>
+    );
 }
 
 export default App;
