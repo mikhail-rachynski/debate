74d4705eddd5cb7f1c93366b4a42aa711f1f8c6a
MIkhail Rachynski <mixasun@gmail.com>
2021-03-21 18:29:14 +0300

    improvements, added icons

--- src/components/Games/Game/Details/Details.jsx
+++ src/components/Games/Game/Details/Details.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
 import style from './Details.module.css'
 import {NavLink} from "react-router-dom";
+import {AiFillEdit, AiOutlineEdit} from "react-icons/ai";
+import {MdDeleteForever} from "react-icons/md";
 
-const Details = (props) => {
-    return (<div className={style.details}>
-        <NavLink to="/game" className={style.topic}>{props.topic}</NavLink>
-        <div className={style.users}>
-            {props.users.length !== 0 ? props.users.map((user, index) =>
-                    <span key={user.id}>{(index? ', ': '')}
-                        <NavLink to={'/profile/' + user.id}>{user.name}</NavLink>
+class Details extends React.Component {
+
+    render() {
+        return (<div className={style.details}>
+            <div className={style.option}>
+                {this.props.currentUserId === this.props.game.creator &&
+                <div>
+                    {this.props.editMode
+                        ? <AiOutlineEdit />
+                        : <AiFillEdit onClick ={this.props.activateEditMode} />}
+                    <MdDeleteForever onClick={this.props.deleteCurrentGame} />
+                </div>}
+            </div>
+            <div className={style.status}>Created: {this.props.game.time}</div>
+            <div className={style.users}>
+                {this.props.users.length !== 0
+                    ? this.props.users.map((user, index) =>
+                        <span key={user.id}>{(index? ', ': '')}
+                            <NavLink to={'/profile/' + user.id}>
+                                {user.name}
+                            </NavLink>
                     </span>)
-                : "Waiting for players"}
-        </div>
-        <span className={style.status}>{props.status}</span>
-        <NavLink to={"/game/" + props.gameId}><button className={style.option}>Enter to game</button></NavLink>
-        {/*<NavLink to="/game"><button disabled={props.status !== "finished"} className={style.option}>Details</button></NavLink>*/}
-    </div>)
+                    : "Waiting for players"}
+            </div>
+        </div>)
+    }
+
+
 }
 
 export default Details
