0483ef92fe0399bce1174493540f101ab52e6554
MIkhail Rachynski <mixasun@gmail.com>
2021-08-07 19:10:43 +0300

    refactoring

--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import style from "./Menu.module.css"
 
-class Menu extends React.Component {
-    render() {
-        return (
-            <div><div className={style.dropdown} >
-                <div id="myDropdown" className={style.dropdownContent} onClick={this.props.onMenuClicked}>
-                    <NavLink to={"/"}>Home</NavLink>
-                    <NavLink to={"/profile"}>Profile</NavLink>
-                    <NavLink to={"/history"}>Game history</NavLink>
-                    <a onClick={this.props.signOut}>Sign Out</a>
-                </div>
+const Menu = ({onMenuClicked, signOut}) => {
+    return (
+        <div><div className={style.dropdown} >
+            <div id="myDropdown" className={style.dropdownContent} onClick={onMenuClicked}>
+                <NavLink to={"/"}>Home</NavLink>
+                <NavLink to={"/profile"}>Profile</NavLink>
+                <NavLink to={"/history"}>Game history</NavLink>
+                <a onClick={signOut}>Sign Out</a>
             </div>
-                <div className="close" onClick={this.props.onMenuClicked}/>
-            </div>
-        )
-    }
+        </div>
+            <div className={style.close} onClick={onMenuClicked}/>
+        </div>
+    )
 }
 
 export default Menu
--- src/components/Header/Menu/MenuContainer.jsx
+++ src/components/Header/Menu/MenuContainer.jsx
@@ -1,37 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {connect} from "react-redux"
 import Menu from "./Menu";
 import profilePhoto from "../../../assets/images/small_profile_photo.jpg";
 import {signOut} from "../../../redux/auth-reducer";
 
 
-class MenuContainer extends React.Component {
-    state = {
-        menuOpened: false
-    }
-    onMenuClicked = () => {
-        if (this.state.menuOpened === true) {
-            this.setState({
-                menuOpened: false
-            })
+const MenuContainer = (props) => {
+    let [dropDownOpened, setDropDownOpened] = useState(false)
+
+    const onMenuClicked = () => {
+        if (dropDownOpened === true) {
+            setDropDownOpened(false)
         } else {
-            this.setState({
-                menuOpened: true
-            })
+            setDropDownOpened(true)
         }
     }
-    render() {
-        return <>
-            {this.state.menuOpened
-                ? <Menu signOut={this.props.signOut} items={this.props.items}
-                        onMenuClicked={this.onMenuClicked}/>
-                : null}
-            <img src={profilePhoto}
-                 alt={"User menu"}
-                 className="dropimg"
-                 onClick={() => { this.onMenuClicked() }}/>
-        </>
-    }
+    return <>
+        {dropDownOpened
+            ? <Menu signOut={props.signOut}
+                    onMenuClicked={onMenuClicked}/>
+            : null}
+        <img src={profilePhoto}
+             alt={"User menu"}
+             className="dropimg"
+             onClick={() => {onMenuClicked()}}/>
+    </>
+
 }
 
 export default connect(null, {signOut})(MenuContainer)
