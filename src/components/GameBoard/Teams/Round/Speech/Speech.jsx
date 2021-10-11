eb90d8eb47257e89abe000d182ad290fda68cf3e
Mikhail Rachynski <mixasun@gmail.com>
2021-10-11 13:59:25 +0200

    added reply & refactoring

--- src/components/GameBoard/Teams/Round/Speech/Speech.jsx
+++ src/components/GameBoard/Teams/Round/Speech/Speech.jsx
@@ -1,52 +1,108 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./Speech.module.css"
 import profilePhoto from "../../../../../assets/images/small_profile_photo.jpg";
 import {MdDeleteForever} from 'react-icons/md';
-import { AiFillEdit,  AiOutlineEdit} from "react-icons/ai";
+import {AiFillEdit} from "react-icons/ai";
+import {TimePassed} from "../../../../common/TimePassed/TimePassed";
+import {BsThreeDots} from "react-icons/bs";
+import Talk from "../Talk/Talk";
 
-class Speech extends React.Component {
-    state = {
-        editMode: false,
-        speech: this.props.speech
-    }
-    deleteSpeech = () => {
-        this.props.deleteSpeech(this.props.speechId)
-    }
-    onSpeechChange = (e) => {
-        this.setState( {
-            speech: e.currentTarget.value
-        })
-    }
-    activateEditMode = () => {
-        this.setState({
-            editMode: true
-        })
-    }
-    deActivateEditMode = () => {
-        this.setState({
-            editMode: false
-        })
-        this.props.updateSpeech(this.props.speechId, this.state.speech)
-    }
-    render() {
+const Speech = (props) => {
+    const [editMode, setEditMode] = useState(false)
+    const [showOptions, setShowOptions] = useState(false)
+    const [speechText, setSpeechText] = useState(props.text)
+    const [reply, setReply] = useState(false)
 
-        return <div className={style.speech}>
-            <img src={profilePhoto} className={style.user} alt={this.props.userName}/>
-            {!this.state.editMode &&
-                <div className={style.text}>{this.props.speech}</div>}
-            {this.state.editMode &&
-                <textarea onChange={this.onSpeechChange}
-                          autoFocus={true}
-                          onBlur={this.deActivateEditMode}
-                          value={this.state.speech} />
-            }
-            {this.props.currentUserId === this.props.userId &&
-                <div className={style.options}>
-                    {this.state.editMode ? <AiOutlineEdit /> : <AiFillEdit onClick ={this.activateEditMode} />}
-                    <MdDeleteForever onClick={this.deleteSpeech} />
-                </div>}
+    const changeReplyEditMode = () => {
+        !reply ? setReply(true) : setReply(false)
+    }
+    const showOptionsMenu = (e) => {
+        e.stopPropagation();
+        setShowOptions(true)
+        document.addEventListener('click', closeOptionsMenu);
+
+    }
+    const closeOptionsMenu = () => {
+        setShowOptions(false)
+        document.removeEventListener('click', closeOptionsMenu);
+    }
+    const deleteSpeech = () => {
+        props.deleteSpeech(props.speechId)
+    }
+    const onSpeechChange = (e) => {
+        setSpeechText(e.currentTarget.value)
+    }
+    const activateEditMode = () => {
+        closeOptionsMenu()
+        setEditMode(true)
+    }
+    const deActivateEditMode = () => {
+        setEditMode(false)
+        props.updateSpeech(props.speechId, speechText)
+    }
+
+    return <div className={style.speech} >
+        <div className={style.header} style={{color: props.team === "government" ? "#64a0a0" : "#a06464"}}>
+            <div className={style.speaker}>
+                <img src={profilePhoto} className={style.avatar} alt={props.userName}/>
+                <div className={style.spoke}>
+                    <span className={style.userName}>{props.userName}</span>
+                    <span className={style.time}> spoke {<TimePassed createdTime={props.created_at}/>}{props.created_at !== props.updated_at && " & updated"}</span>
+                </div>
+            </div>
+            {props.currentUserId === props.userId &&
+            <div className={style.options} onClick={showOptionsMenu}>
+                {showOptions
+                    ? <div className={style.optionsMenu}>
+                        <AiFillEdit onClick={activateEditMode} />
+                        <MdDeleteForever onClick={deleteSpeech} />
+                    </div> :
+                    <BsThreeDots/>}
+            </div>}
         </div>
-    }
+        {!editMode &&
+        <div className={style.text}>{props.text}</div>}
+
+        {editMode &&
+        <textarea onChange={onSpeechChange}
+                  autoFocus={true}
+                  onBlur={deActivateEditMode}
+                  value={speechText} />
+        }
+
+        <div className={style.footer}>
+            {reply
+                ? <Talk roundId={props.roundId}
+                        reply={props.text}
+                        speechId={props.speechId}
+                        changeEditMode={changeReplyEditMode}
+                        pushSpeech={props.pushSpeech}/>
+                : <div className={style.replyButton} onClick={changeReplyEditMode}>Reply</div>}
+
+        </div>
+
+        {props.replies &&
+        <div className={style.reply}>
+            {props.replies.map(speech => <Speech key={speech.id}
+                                                 roundId={props.roundId}
+                                                 replies={props.speeches.filter(reply => reply.reply_to === speech.id)}
+                                                 speeches={props.speeches}
+                                                 speechId={speech.id}
+                                                 text={speech.text}
+                                                 created_at={speech.created_at}
+                                                 updated_at={speech.updated_at}
+                                                 userId={speech.user_id}
+                                                 userName={speech.user_name}
+                                                 team={props.team}
+                                                 currentUserId={props.currentUserId}
+                                                 pushSpeech={props.pushSpeech}
+                                                 updateSpeech={props.updateSpeech}
+                                                 deleteSpeech={props.deleteSpeech}
+            />)}
+        </div>
+        }
+
+    </div>
 }
 
 export default Speech
--- src/components/GameBoard/Teams/Round/Speech/SpeechContainer.jsx
+++ src/components/GameBoard/Teams/Round/Speech/SpeechContainer.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {connect} from "react-redux";
 import Speech from "./Speech";
-import {deleteSpeech, updateSpeech} from "../../../../../redux/game-reducer";
+import {deleteSpeech, pushSpeech, updateSpeech} from "../../../../../redux/game-reducer";
 
 function SpeechContainer(props) {
     const [speeches, setSpeeches] = useState([])
@@ -11,15 +11,24 @@ function SpeechContainer(props) {
     }, [props.speeches])
     return <div>
         {speeches &&
-        speeches.map(speech => {
+        speeches.filter(speech => !speech.reply_to).map(speech => {
             return <div key={speech.id}>
-                <Speech speechId={speech.id}
-                        speech={speech.speech}
-                        userId={speech.user_id}
-                        userName={speech.user_name}
-                        currentUserId={props.currentUserId}
-                        updateSpeech={props.updateSpeech}
-                        deleteSpeech={props.deleteSpeech}/>
+                <Speech
+                    roundId={props.roundId}
+                    speeches={props.speeches}
+                    replies={props.speeches.filter(reply => reply.reply_to === speech.id)}
+                    speechId={speech.id}
+                    text={speech.text}
+                    created_at={speech.created_at}
+                    updated_at={speech.updated_at}
+                    userId={speech.user_id}
+                    userName={speech.user_name}
+                    team={props.team}
+                    currentUserId={props.currentUserId}
+                    pushSpeech={props.pushSpeech}
+                    updateSpeech={props.updateSpeech}
+                    deleteSpeech={props.deleteSpeech}
+                />
             </div>
         })
         }</div>
@@ -34,4 +43,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps, {deleteSpeech, updateSpeech})(SpeechContainer)
+export default connect(mapStateToProps, {pushSpeech, deleteSpeech, updateSpeech})(SpeechContainer)
